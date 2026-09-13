719a72b5d33af76f5559740095801073f1523464
beecheverriap-beep <be.echeverriap@duocuc.cl>
2026-09-12 23:04:58 -0300

    Primer comentario

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public  class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese su edad: ");
+        int edad = scanner.nextInt();
+        System.out.println("La edad es de: " + edad);
+    }
+}

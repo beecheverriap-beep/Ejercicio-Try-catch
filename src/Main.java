af09950f41ab883cf678e9902164316af063a91e
beecheverriap-beep <be.echeverriap@duocuc.cl>
2026-09-13 18:32:48 -0300

    Segundo Mensaje

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public  class Main {
     public static void main(String[] args) {
 
+        try {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ingrese su edad: ");
         int edad = scanner.nextInt();
         System.out.println("La edad es de: " + edad);
+        } catch (InputMismatchException e) {
+            System.out.println("Error: Ingresa un numero");
+        }
     }
 }
